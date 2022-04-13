2bd4016f8d2850752fc113db282a81f1527f213f
George C. Huntington III <ghuntington@hubbell.com>
2022-04-13 14:48:12 -0400

    clean up to make more rust-y

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-use chrono::{NaiveDate};
+use chrono::{NaiveDate, DateTime, Utc};
 use log_parse::*;
 
+
+use std::path::PathBuf;
 use std::{fs::File, collections::HashMap};
 use std::io::{prelude::*, BufReader, self};
 
 use flate2::read::GzDecoder;
 use glob::glob;
 
+
+
+
 fn main() {
 
     let mut players: Players = HashMap::new();
 
 
-    for entry in glob("./logs/*.log.gz").expect("no files") {
+    for entry in glob("./logs/*.log*").expect("no files") {
         if let Ok(path) = entry {
-            let fname = path.file_name().unwrap();
-            let (_, mut date) = parse_datelike(fname.to_str().unwrap()).unwrap();
+            let mut date = extract_date_from_path(&path);            
+            
             let display = path.display();
             let file = match File::open(&path) {
                 Err(why) => panic!("couldn't open {}: {}", display, why),
                 Ok(file) => file,
             };
-        
-            let mut reader = BufReader::new(GzDecoder::new(file));
-            
+
+            let mut reader = get_reader(path.extension().unwrap() == "gz", file);
             extract_player_data(&mut players, &mut reader, &mut date).unwrap();
         } else {
             todo!()
@@ -41,6 +45,39 @@ fn main() {
             }
         }
     }
+}
+
+fn extract_date_from_path(path: &PathBuf) -> NaiveDate{
+    let fname = match path.file_name() {
+        Some(fname) => fname.to_str().unwrap(),
+        None => panic!("no file name"),
+    };
+
+    let date = match parse_datelike(fname) {
+        Ok((_,date)) => date,
+        Err(_e) => match path.metadata() {
+            Ok(metadata) => {
+                if let Ok(created) = metadata.created() {
+                    let date_time = DateTime::<Utc>::from(created);
+                    let date: NaiveDate = date_time.date().naive_utc();
+                    date
+                } else {
+                    panic!("something bad happenned")
+                }
+            },
+            Err(x) => panic!("couldn't get metadata: {}", x),
+        },
+    };
+    date
+}
+
+fn get_reader(gz: bool, file: File) -> Box<dyn BufRead> {
+    if gz {
+        Box::new(BufReader::new(GzDecoder::new(file)))
+    } else {
+        Box::new(BufReader::new(file))
+    }
+
 }
 
 fn extract_player_data<R: BufRead>(players: &mut Players, reader: &mut R, date: &mut NaiveDate) -> io::Result<()> {
